d3ad7e9d5b1d6683f8e8101132b0519c58e21a71
Kevin <kevinjh775@gmail.com>
2026-01-26 16:40:52 -0300

    Mensagem de boas vindas

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AuthenticationService } from 'src/app/core/services/auth.service';
 
 @Component({
   standalone: true,
@@ -8,5 +9,13 @@ import { CommonModule } from '@angular/common';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  userName: string = '';
+
+  constructor(private authService: AuthenticationService) {}
+
+  ngOnInit(): void {
+    const user: any = this.authService.getCurrentUser();
+    this.userName = user?.name || user?.fullName || user?.given_name || 'Usuário';
+  }
 }
